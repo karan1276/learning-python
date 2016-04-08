1ddc48d30d95520e26c2df1dd68316adead4f701
Karan Sharma <karan1276@gmail.com>
2016-04-08 15:02:39 +0530

    Using list as queue

---
+++ data structures/listasqueue.py
@@ -0,0 +1,12 @@
+nums = [2,7,4,9,1,6,0]
+print nums
+
+# Enqueue , adding element
+print "nums.append(3)"
+nums.append(3)
+print nums
+
+# Dequeue , removing elements
+print "nums.pop(0)"
+nums.pop(0)
+print nums
