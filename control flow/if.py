765ffc595d7ffdf27721d9baaccbeacdfe6bdec3
Karan Sharma <karan1276@gmail.com>
2016-04-08 11:05:34 +0530

    tryin if statement

---
+++ control flow/if.py
@@ -0,0 +1,10 @@
+x = int(raw_input("Please enter an integer: "))
+
+if x<0:
+    print "NUmber you entered is less than zero"
+elif x==0:
+    print "Ur are at zero"
+elif x==1:
+    print "One and only one"
+else:
+    print "You are unpredictable"
